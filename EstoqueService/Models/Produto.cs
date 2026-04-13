29def8b00ffee5b45bc8e60f21945b24bf84b9e3
dev-kaio <kaiomanfro2@gmail.com>
2026-04-13 11:39:35 -0300

    feat: adiciona models e configura persistência com EF Core e SQLite

---
+++ EstoqueService/Models/Produto.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Produto
+{
+    public int Id { get; set; } // pk
+    [Required]
+    [MinLength(1)]
+    public string Codigo { get; set; }
+    [Required]
+    public string Descricao { get; set; } // nome do produto
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Saldo { get; set; } // qtde disponível em estoque
+
+
+    public Produto(int id, string codigo, string descricao, int saldo)
+    {
+        Id = id;
+        Codigo = codigo;
+        Descricao = descricao;
+        Saldo = saldo;
+    }
+
+    public Produto()
+    {
+    }
+}
