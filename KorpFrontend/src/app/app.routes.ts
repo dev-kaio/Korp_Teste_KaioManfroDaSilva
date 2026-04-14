e2d2ae67c24d54c96d787db563bf70ff8fd1b990
dev-kaio <kaiomanfro2@gmail.com>
2026-04-14 14:20:32 -0300

    feat: cria components e adiciona caminho nas rotas

--- KorpFrontend/src/app/app.routes.ts
+++ KorpFrontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotasComponent } from './pages/notas-component/notas-component';
+import { ProdutosComponent } from './pages/produtos-component/produtos-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'notas', component: NotasComponent },
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+];
---
+++ KorpFrontend/src/app/pages/produtos-component/produtos-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos-component',
+  imports: [],
+  templateUrl: './produtos-component.html',
+  styleUrl: './produtos-component.css',
+})
+export class ProdutosComponent {}
